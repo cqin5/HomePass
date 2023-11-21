6776efd935242e8bceecdad856c040274e108572
Chuhan Qin <frank@mary.ag>
2023-11-21 21:09:41 +0800

    Update

--- HomePass/AddPassView.swift
+++ HomePass/AddPassView.swift
@@ -9,27 +9,98 @@ import Foundation
 import SwiftUI
 
 struct AddPassView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-    @State private var name: String = ""
-    @State private var details: String = ""
-    @State private var image: UIImage?
-    // ... other state variables for form fields
+    @Environment(\.presentationMode) var presentationMode
+        @State private var name: String = ""
+        @State private var details: String = ""
+        @State private var image: UIImage?
+        @State private var showingImagePicker = false
+        var onSave: ([HomePass]) -> Void  // Closure to be called after saving
 
-    var body: some View {
-        NavigationView {
-            Form {
-                TextField("Name", text: $name)
-                TextField("Details", text: $details)
-                // Image Picker Here
-                Button("Save", action: savePass)
+        var body: some View {
+            NavigationView {
+                Form {
+                    Section {
+                        TextField("Name", text: $name)
+                            .frame(height: 40)
+                        TextField("Details", text: $details)
+                            .frame(height: 40)
+                    }
+                    
+                    Section {
+                        if let image = image {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 300) // Adjusted height for a larger image display
+                        }
+                    }
+                    
+                    Section {
+                        Button("Choose Image") {
+                            showingImagePicker = true
+                        }
+                        .frame(height: 40, alignment: .center)
+                        
+                    }
+
+                    Section {
+                        Button("Save", action: savePass)
+                            .frame(height: 40, alignment: .center)
+                    }
+                }
+                .navigationBarTitle("Add Pass", displayMode: .inline)
+                .sheet(isPresented: $showingImagePicker) {
+                    ImagePicker(image: $image)
+                }
             }
-            .navigationBarTitle("Add Pass", displayMode: .inline)
         }
-    }
 
     private func savePass() {
         let newPass = HomePass(name: name, details: details, image: image)
-        
-        // Handle navigation back to the list
+        var currentPasses = loadPassesFromUserDefaults()
+        currentPasses.append(newPass)
+        savePassesToUserDefaults(currentPasses)
+        onSave(currentPasses)
+        presentationMode.wrappedValue.dismiss() // Dismiss the view
+    }
+
+    private func loadPassesFromUserDefaults() -> [HomePass] {
+        // Your logic to load passes from UserDefaults
+        loadPasses()
+    }
+
+    private func savePassesToUserDefaults(_ passes: [HomePass]) {
+        // Your logic to save passes to UserDefaults
+        savePasses(passes)
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            picker.dismiss(animated: true)
+        }
     }
 }
--- HomePass/PassesListView.swift
+++ HomePass/PassesListView.swift
@@ -10,67 +10,105 @@ import SwiftUI
 
 struct PassesListView: View {
     @State private var passes: [HomePass] = []
-
+    @State private var showingAddPassView = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(passes, id: \.id) { pass in
-                    HStack {
-                        if let image = UIImage(data: pass.imageData ?? Data()) {
-                            Image(uiImage: image)
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
+            NavigationView {
+                Group {
+                    if passes.isEmpty {
+                        // Display a message when there are no passes
+                        Text("No passes yet.")
+                            .foregroundColor(.gray)
+                            .opacity(0.5)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    } else {
+                        // List of passes
+                        List {
+                            ForEach(passes, id: \.id) { pass in
+                                PassRow(pass: pass)
+                                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                            }
+                            .onDelete(perform: deletePasses)
                         }
-                        VStack(alignment: .leading) {
-                            Text(pass.name)
-                                .font(.headline)
-                            let details = pass.details
-                            Text(details)
-                                .font(.subheadline)
-                            
-                        }
+                        .backgroundStyle(.clear)
                     }
                 }
-                .onDelete(perform: deletePasses)
+                .navigationBarTitle("My Passes")
+                .navigationBarItems(trailing: Button(action: {
+                    showingAddPassView = true
+                }) {
+                    Image(systemName: "plus")
+                })
             }
-            .navigationBarTitle("My Passes")
-            .navigationBarItems(trailing: Button(action: {
-                // Implement navigation to Add Pass View
-            }) {
-                Image(systemName: "plus")
-            })
+            .sheet(isPresented: $showingAddPassView) {
+                AddPassView(onSave: { _ in
+                    loadPasses()
+                })
+            }
             .onAppear {
                 loadPasses()
             }
         }
-    }
+    
+    
+    struct PassRow: View {
+        let pass: HomePass
 
-    private func loadPasses() {
-        passes = loadPassesFromUserDefaults()
-//        let _ = ForEach(passes, id: \.id) { pass in
-//            HStack {
-//                if let imageData = pass.imageData, let image = UIImage(data: imageData) {
-//                    Image(uiImage: image)
-//                        .resizable()
-//                        .frame(width: 50, height: 50)
-//                        .clipShape(RoundedRectangle(cornerRadius: 10))
-//                }
-//                VStack(alignment: .leading) {
-//                    Text(pass.name)
-//                        .font(.headline)
+        var body: some View {
+            ZStack(alignment: .bottomLeading) {
+                // Image
+                if let imageData = pass.imageData, let image = UIImage(data: imageData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 200) // Adjust height as needed
+                        .cornerRadius(15)
+                        .clipped()
+                }
+
+                // Transparent gradient
+                LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.7)]), startPoint: .top, endPoint: .bottom)
+                    .cornerRadius(15)
+
+                // Text
+                VStack(alignment: .leading) {
+                    Text(pass.name)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
 //                    Text(pass.details)
 //                        .font(.subheadline)
-//                }
-//            }
-//        }
+//                        .foregroundColor(.white)
+                }
+                .padding()
+            }
+            .frame(height: 200) // Ensuring the ZStack has a fixed height
+            .shadow(radius: 5)
+            .padding(.horizontal)
+        }
     }
-
+    
+    
+    private var addButton: some View {
+        Button(action: {
+            // Placeholder action for adding a new pass
+            // In a real app, this should navigate to a view for adding a new pass
+            print("Add Pass Button Tapped")
+        }) {
+            Image(systemName: "plus")
+        }
+    }
+    
+    
+    private func loadPasses() {
+        passes = loadPassesFromUserDefaults()
+    }
+    
     private func deletePasses(at offsets: IndexSet) {
         passes.remove(atOffsets: offsets)
         savePassesToUserDefaults(passes)
     }
-
+    
     private func loadPassesFromUserDefaults() -> [HomePass] {
         if let savedData = UserDefaults.standard.data(forKey: "HomePasses") {
             let decoder = JSONDecoder()
@@ -80,14 +118,13 @@ struct PassesListView: View {
         }
         return []
     }
-
+    
     private func savePassesToUserDefaults(_ passes: [HomePass]) {
         let encoder = JSONEncoder()
         if let encoded = try? encoder.encode(passes) {
             UserDefaults.standard.set(encoded, forKey: "HomePasses")
         }
     }
-
 }
 
 
